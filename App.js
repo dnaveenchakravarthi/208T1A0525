351c1d5e32400ad2614c62567af3739973df0682
dnaveenchakravarthi <114055115+dnaveenchakravarthi@users.noreply.github.com>
2023-08-19 11:48:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>"numbers":[1,2,3,5,,7,8,9,11,13,15,17,19,21,23]</p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
